19d0b78a2c49661faed998d74a40d6aa23ca2a8b
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2020-11-12 23:33:03 -0500

    Improved launching

--- Word splitting/Assets/Scripts/TextSplitter.cs
+++ Word splitting/Assets/Scripts/TextSplitter.cs
@@ -18,6 +18,7 @@ public class TextSplitter : MonoBehaviour
     private float rotationSpeed;
 
     private TextMeshPro textObj;
+    private bool launched = false;
 
     private Controls.PlayerActions input;
     // Start is called before the first frame update
@@ -33,11 +34,26 @@ public class TextSplitter : MonoBehaviour
     {
         if (textObj != null)
         {
-            split();
+            if (!launched)
+            {
+                launched = true;
+                //textObj.GetComponent<FixedJoint2D>().connectedBody = null;
+                textObj.GetComponent<FixedJoint2D>().enabled = false;
+                Rigidbody2D r = textObj.GetComponent<Rigidbody2D>();
+                r.simulated = true;
+                r.gravityScale = .5f;
+                r.AddForce(-textObj.transform.up * 5, ForceMode2D.Impulse);
+            }
+            else
+            {
+                split();
+            }
         }
         else
         {
             textObj = Instantiate(sentencePrefab, spawnPos.position + new Vector3(0, -1), Quaternion.identity);
+            textObj.GetComponent<FixedJoint2D>().connectedBody = spawnerRoot.GetComponent<Rigidbody2D>();
+            textObj.GetComponent<Rigidbody2D>().gravityScale = 0;
         }
     }
 
@@ -45,12 +61,11 @@ public class TextSplitter : MonoBehaviour
     void Update()
     {
         float angle = Mathf.Sin(Time.time * rotationSpeed) * 45; //tweak this to change frequency
-
         spawnerRoot.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        if (textObj != null)
+        if (textObj != null && !launched)
         {
             textObj.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            //textObj.transform.position = Vector3
+            textObj.transform.position = spawnPos.position - spawnPos.up;
         }
     }
     private void split()
@@ -68,9 +83,11 @@ public class TextSplitter : MonoBehaviour
             characterTMP.rectTransform.position = rect.center;
             characterTMP.rectTransform.sizeDelta = new Vector2(rect.width, rect.height);
             characterTMP.GetComponent<BoxCollider2D>().size = new Vector2(rect.width, rect.height);
+            characterTMP.GetComponent<Rigidbody2D>().velocity = textObj.GetComponent<Rigidbody2D>().velocity;
             //characterTMP.gameObject.AddComponent<BoxCollider2D>();
             //characterTMP.gameObject.AddComponent<Rigidbody2D>();
         }
+        launched = false;
         Destroy(textObj.gameObject);
     }
 
